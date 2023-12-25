061ef10ebd97cf6c2dbfd2652fa4f1c21596ffca
khaledtook23 <154600649+khaledtook23@users.noreply.github.com>
2023-12-25 19:16:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ const scanButton = document.getElementById('scanButton');
 const qrCanvas = document.getElementById('qr-canvas');
 
 scanButton.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }) // Specify back camera
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
     .then(stream => {
       qrCanvas.getContext('2d').fillRect(0, 0, qrCanvas.width, qrCanvas.height); // Clear canvas
       qrCanvas.style.display = 'block'; // Show the canvas
+
+      // Adjust canvas size for mobile screens
+      qrCanvas.width = window.innerWidth;
+      qrCanvas.height = window.innerHeight - scanButton.offsetHeight - 40; // Account for button and margins
 
       Quagga.decodeSingle({
         locate: true,
         decoder: { readers: ['code_128_reader'] },
         inputStream: stream,
+        // Potentially adjust options for mobile performance
       }).then(result => {
-        const url = result.codeResult.code;
-        new Audio('beep.mp3').play(); // Play a sound
-
-        // Perform actions with the URL, e.g., open it in a new tab
-        window.open(url, '_blank');
-
-        qr
+        // ... (rest of the code
