efd85b6bb247d09ace89d5871241f492936d8d04
khaledtook23 <154600649+khaledtook23@users.noreply.github.com>
2023-12-25 22:20:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-const scanButton = document.getElementById('scanButton');
-const qrCanvas = document.getElementById('qr-canvas');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const video = document.getElementById('video');
+  const scanResult = document.getElementById('scan-result');
 
-scanButton.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-    .then(stream => {
-      qrCanvas.getContext('2d').fillRect(0, 0, qrCanvas.width, qrCanvas.height); // Clear canvas
-      qrCanvas.style.display = 'block'; // Show the canvas
-
-      // Adjust canvas size for mobile screens
-      qrCanvas.width = window.innerWidth;
-      qrCanvas.height = window.innerHeight - scanButton.offsetHeight - 40; // Account for button and margins
-
-      Quagga.decodeSingle({
-        locate: true,
-        decoder: { readers: ['code_128_reader'] },
-        inputStream: stream,
-      }).then(result => {
-        const url = result.codeResult.code;
-        new Audio('beep.mp3').play(); // Play a sound
-
-        // Perform actions with the URL, e.g., open it in a new tab
-        window.open(url, '_blank');
-
-        qrCanvas.style.display = 'none'; // Hide the canvas
+  // Check if the browser supports mediaDevices and getUserMedia
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+        scanQRCode();
       })
-      .catch(err => {
-        console.error('Error decoding QR code:', err);
-        // Handle errors, e.g., display an error message
-      })
-      .finally(() => {
-        stream.getTracks().forEach(track => track.stop()); // Stop camera stream after scanning
+      .catch((error) => {
+        console.error('Error accessing camera:', error);
       });
-    })
-    .catch(error => {
-      console.error('Error accessing camera:', error);
-      // Handle camera access errors, e.g., display an error message
-    });
-});
+  } else {
+    console.error('getUserMedia is not supported in this browser');
+  }
 
-// Handle window resizing for responsive canvas
-window.addEventListener('resize', () => {
-  qrCanvas.width = window.innerWidth;
-  qrCanvas.height = window.innerHeight - scanButton.offsetHeight - 40;
+  function scanQRCode() {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    function scan() {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+      if (code) {
+        scanResult.textContent = 'Scanned QR Code: ' + code.data;
+
+        // Execute the URL (replace with your logic)
+        window.location.href = code.data;
+
+        // Clear the result after a delay (adjust as needed)
+        setTimeout(() => {
+          scanResult.textContent = '';
+        }, 3000);
+      }
+
+      requestAnimationFrame(scan);
+    }
+
+    scan();
+  }
 });
