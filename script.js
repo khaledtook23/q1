e962234ec397a2eb6b81ecb47814ebf27e86ca3d
khaledtook23 <154600649+khaledtook23@users.noreply.github.com>
2023-12-25 19:23:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const scanButton = document.getElementById('scanButton');
-const qrCanvas = document.getElementById('qr-canvas');
-
-scanButton.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-    .then(stream => {
-      qrCanvas.getContext('2d').fillRect(0, 0, qrCanvas.width, qrCanvas.height); // Clear canvas
-      qrCanvas.style.display = 'block'; // Show the canvas
-
-      // Adjust canvas size for mobile screens
-      qrCanvas.width = window.innerWidth;
-      qrCanvas.height = window.innerHeight - scanButton.offsetHeight - 40; // Account for button and margins
-
-      Quagga.decodeSingle({
-        locate: true,
-        decoder: { readers: ['code_128_reader'] },
-        inputStream: stream,
-        // Potentially adjust options for mobile performance
-      }).then(result => {
-        // ... (rest of the code
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QR Code Scanner</title>
+  <link rel="stylesheet" href="styles.css">
+  <script src="https://cdn.rawgit.com/serratus/quaggaJS/master/dist/quagga.min.js"></script>
+</head>
+<body>
+  <button id="scanButton">Scan QR Code</button>
+  <canvas id="qr-canvas"></canvas>
+  <script src="script.js"></script>
+</body>
+</html>
