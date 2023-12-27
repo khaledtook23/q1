7f86c82ce54bd5b23fb9797b4f707f7ea70024fd
khaledtook23 <154600649+khaledtook23@users.noreply.github.com>
2023-12-27 20:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-// script.js
-
-// Function to handle the QR code scanning
-function onScanSuccess(qrCodeMessage) {
-  // Display the scanned URL
-  document.getElementById('result').innerHTML = `<p>Scanned URL: <a href="${qrCodeMessage}" target="_blank">${qrCodeMessage}</a></p>`;
-
-  // Open the scanned URL in a new tab/window
-  window.open(qrCodeMessage, '_blank');
-
-  // Pause for a moment (you can adjust the delay)
-  setTimeout(() => {
-    // Clear the result container
-    document.getElementById('result').innerHTML = '';
-
-    // Start scanning again
-    html5QrcodeScanner.start();
-  }, 2000); // 2 seconds delay before scanning again
-}
-
-// Initialize the QR code scanner
-const html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader", // Reader container id
-  { fps: 10, qrbox: 250 } // Optional parameters
-);
-
-// Start the QR code scanner
-html5QrcodeScanner.render(onScanSuccess);
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+  window.location.href = content;
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[0]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
